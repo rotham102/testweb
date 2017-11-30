8b72ae3fbd865c5c6d49ca3631bb09738e7aff4a
Unknown <rothie102@gmail.com>
2017-11-29 23:32:16 -0500

    Remove bullshit

--- index.view.php
+++ index.view.php
@@ -8,7 +8,7 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!--#-->
-	<title>Amanda M. Roth <?php for($i = 0; $i < 200; $i++) echo ';)'; ?></title>
+	<title>Amanda M. Roth</title>
 </head>
 
 <body>
