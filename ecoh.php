e7b48e9d95a51ba7ecbb3df3010e87c82e45b62a
Unknown <rothie102@gmail.com>
2017-12-12 00:07:18 -0500

    i'm dying

--- ecoh.php
+++ ecoh.php
@@ -5,14 +5,14 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="https://use.typekit.net/rpu5kiz.css">
 	<link rel="stylesheet" type="text/css" href="styles/styles.css">
-	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600" rel="stylesheet">
 	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.png" />
 	<script src="styles/scripts.js"></script>
 
 	<title>Amanda M. Roth</title>
 </head>
 <body>
-  <div class="wrapper-works">
+  <div class="wrapper">
 <!--NAV-->
     <div class="nav">
 	  <div id="mySidenav" class="sidenav">
@@ -29,16 +29,16 @@
         <div class="work-desc">
           <div class="nav-afterspace"></div>
             <h1>Emmanuel City of Hope</h1>
-            <p> This was a branding and web design project for a newly established ministry in my community. The clients envisioned a modern logo design with subtle religious imagery, and requested that it feature deep pinks and blues. Their website is currently under construction.</p>
+            <p> This was a <strong>branding</strong> and <strong>web design</strong> project for a newly established ministry in my community. The clients envisioned a modern logo design with subtle religious imagery, and requested that it feature deep pinks and blues. Their website is currently under construction.</p>
             <hr/>
             <button>NEXT PROJECT</button>
         </div>
         <div class="work-imgs">
-               <div class="nav-afterspace-imgs"></div>
-                <img src="images/ecoh-cards.jpg">
-                <img src="images/ecoh-logo-white.jpg">
-                <img src="images/ecoh-logo-blue.jpg">
+                <div class="nav-afterspace-imgs"></div>
+                        <img src="images/ecoh-cards.jpg">
+                        <img src="images/ecoh-logo-white.jpg">
+                        <img src="images/ecoh-logo-blue.jpg">   
         </div>
-        
+<div class="large-screen-border"></div>        
   </div>
 </body>
--- index.view.php
+++ index.view.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="https://use.typekit.net/rpu5kiz.css">
 	<link rel="stylesheet" type="text/css" href="styles/styles.css">
-	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600" rel="stylesheet">
 	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.png" />
 	<script src="styles/scripts.js"></script>
 
