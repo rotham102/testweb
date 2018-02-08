5673be927bd0bf22c6493bfe92d1f96ffbdc7f2a
Unknown <rothie102@gmail.com>
2018-02-07 23:03:44 -0500

    added hiv sex worker motion graphic vid

--- index.php
+++ index.php
@@ -151,7 +151,7 @@ $arr = [
 		'works' => [
 		],
 		'video' => [
-            '<iframe width="800" height="450" src="https://www.youtube.com/watch?v=0L8MYDUgv7A" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>'
+            '<iframe width="560" height="315" src="https://www.youtube.com/embed/0L8MYDUgv7A?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
                 ]
     ]
     ];
